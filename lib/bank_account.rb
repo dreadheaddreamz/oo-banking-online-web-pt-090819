2430606be30ac5157ede566e67d07c1d352e7c73
James Batsell <jamesbatsell05@gmail.com>
2019-10-09 03:57:14 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,10 +1,11 @@
 class BankAccount
   
-  attr_accessor :amount
+  attr_accessor :amount, :status
   attr_reader :name
   
   def initialize(name)
     @amount = 1000
+    @status = "open"
    end
   
   def initialize(name)
