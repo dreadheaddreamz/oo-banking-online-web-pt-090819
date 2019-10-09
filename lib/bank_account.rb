3e647da7f08bb21223808cb3d73a67eb44573931
James Batsell <jamesbatsell05@gmail.com>
2019-10-09 03:52:54 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -3,9 +3,8 @@ class BankAccount
   attr_accessor :account, :name 
   
   def initialize(account)
-    @account = account
-    @conditon = 1000
-  end
+    @account = 1000
+   end
   
   def initialize(name)
     @name = name
